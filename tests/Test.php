5180d8b034b88373f6090ddbed8f48f033962e5b
Turbanov Valeriy <tur.4000@gmail.com>
2020-10-08 13:31:20 +0300

    generator() function

--- tests/Test.php
+++ tests/Test.php
@@ -8,6 +8,7 @@ use function Php\Immutable\Fs\Trees\trees\mkdir;
 use function Php\Immutable\Fs\Trees\trees\mkfile;
 use function Php\Immutable\Fs\Trees\trees\getChildren;
 use function App\removeFirstLevel\removeFirstLevel;
+use function App\generator\generator;
 use function App\du\du;
 use function App\du\getDirSize;
 
@@ -33,6 +34,85 @@ class Test extends TestCase
         $tree = [[1, [3, 2]], 2, [3, 5], 2, [130, [1, [550, 10]]]];
         $result = [1, [3, 2], 3, 5, 130, [1, [550, 10]]];
         $this->assertEquals($result, removeFirstLevel($tree));
+    }
+
+    // test generator()
+    public function testGenerator()
+    {
+        $tree = [
+            'name' => 'php-package',
+            'children' => [
+                ['name' => 'Makefile', 'meta' => [], 'type' => 'file'],
+                ['name' => 'README.md', 'meta' => [], 'type' => 'file'],
+                [
+                    'name' => 'dist',
+                    'children' => [],
+                    'meta' => [],
+                    'type' => 'directory'
+                ],
+                [
+                    'name' => 'tests',
+                    'children' => [
+                        [
+                            'name' => 'test.php',
+                            'meta' => ['type' => 'text/php'],
+                            'type' => 'file'
+                        ]
+                    ],
+                    'meta' => [],
+                    'type' => 'directory'
+                ],
+                [
+                    'name' => 'src',
+                    'children' => [
+                        [
+                            'name' => 'index.php',
+                            'meta' => ['type' => 'text/php'],
+                            'type' => 'file'
+                        ]
+                    ],
+                    'meta' => [],
+                    'type' => 'directory'
+                ],
+                [
+                    'name' => 'phpunit.xml',
+                    'meta' => ['type' => 'text/xml'],
+                    'type' => 'file'
+                ],
+                [
+                    'name' => 'assets',
+                    'children' => [
+                        [
+                            'name' => 'util',
+                            'children' => [
+                                [
+                                    'name' => 'cli',
+                                    'children' => [
+                                        [
+                                            'name' => 'LICENSE',
+                                            'meta' => [],
+                                            'type' => 'file'
+                                        ]
+                                    ],
+                                    'meta' => [],
+                                    'type' => 'directory'
+                                ]
+                            ],
+                            'meta' => [],
+                            'type' => 'directory'
+                        ]
+                    ],
+                    'meta' => ['owner' => 'root', 'hidden' => false],
+                    'type' => 'directory'
+                ]
+            ],
+            'meta' => ['hidden' => true],
+            'type' => 'directory'
+        ];
+
+        $actual = generator();
+        
+        $this->assertEquals($tree, $actual);
     }
 
     // test du()
